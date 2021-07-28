789604c666e3166fbb14c983bc6c95106d97140e
Hayden Huynh <huynhhai323@gmail.com>
2021-07-28 19:14:59 +0700

    Cleaned up Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,12 @@
-const Home = () => {
-  // By default, event handling function automatically receives an Event object as its first parameter
-  const handleClick = (e) => {
-    console.log("Hello ninjas", e);
-  };
+import { useState } from "react";
 
-  const handleClickAgain = (name, e) => {
-    console.log(`Hello ${name}`, e.target);
-  };
+const Home = () => {
+  const handleClick = (e) => {};
 
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click Me</button>
-
-      {/* Usage of anonymous function to avoid calling the named function immediately */}
-      {/* Anonymous function automatically receives the Event object as first parameter */}
-      <button
-        onClick={(e) => {
-          handleClickAgain("Mario", e);
-        }}
-      >
-        Click Me Again
-      </button>
     </div>
   );
 };

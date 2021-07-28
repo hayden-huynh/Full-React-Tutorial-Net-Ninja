d7e81949c9da9565cdd988c161a3535f3c187928
Hayden Huynh <huynhhai323@gmail.com>
2021-07-28 20:42:55 +0700

    Finished tutorial 8

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,22 @@
 import { useState } from "react";
 
 const Home = () => {
-  const handleClick = (e) => {};
+  // useState returns an array of the current value and the function to set a new value
+  // Here we are using array destructuring to retrieve the value and the function reference
+  const [name, setName] = useState("Mario");
+  const [age, setAge] = useState(25);
+
+  const handleClick = (e) => {
+    setName("Luigi");
+    setAge(30);
+  };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleClick}>Click Me</button>
     </div>
   );

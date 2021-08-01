350d4bd39344cb436b7aa7848cd620be49e1f5aa
Hayden Huynh <huynhhai323@gmail.com>
2021-08-01 22:26:54 +0700

    Finished tutorial 23

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
-          // Inline stylings
-          // style={{
-          //   color: "white",
-          //   backgroundColor: "#f1356d",
-          //   borderRadius: "8px",
-          // }}
-        >
-          New Blog
-        </a>
+        {/* Use Link component so that React can intercept the request sent to the server for content */}
+        {/* By doing this, when we click on these links, the request will be sent to React and not the backend server. React will then look at what route is requested and serve the according content, without the need for the server to re-send the html file */}
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </nav>
   );

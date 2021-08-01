02fb84f91964f454e96c6af76091db7022be7651
Hayden Huynh <huynhhai323@gmail.com>
2021-08-01 21:38:52 +0700

    Finished tutorial 19

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+// Custom hook is also a function
+// The function name of a custom hook must start with "use" otherwise it cannot be used
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;

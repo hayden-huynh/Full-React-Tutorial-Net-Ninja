7f7cd8e5389775a9556d437992ac43cb9ecaa598
Hayden Huynh <huynhhai323@gmail.com>
2021-08-02 14:20:34 +0700

    Finished tutorial 27

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario")
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title} // Set the value of the input to equal the state
+          onChange={(event) => setTitle(event.target.value)} // Update the state on every changes made to the value of the input
+        />
+        <label>Blog body:</label>
+        <textarea 
+          required
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select 
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
-}
- 
+};
+
 export default Create;

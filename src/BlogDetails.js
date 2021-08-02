9133224c109bb2089aa4e54e7b2d296155e92241
Hayden Huynh <huynhhai323@gmail.com>
2021-08-02 15:03:48 +0700

    Finished tutorial 31

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,14 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
 
   const {
     data: blog,
     error,
     isPending,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -19,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>

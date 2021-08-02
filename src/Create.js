8ea8d767491bf408cb45a9e9b4b1494dc33b40b9
Hayden Huynh <huynhhai323@gmail.com>
2021-08-02 14:54:28 +0700

    Finished tutorial 30

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
@@ -6,14 +7,14 @@ const Create = () => {
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
 
+  // The useHistory hook returns a object that can be manipulated to go back and forth in the history of visited pages, similar to using th back and forward buttons in the browser
+  const history = useHistory();
+
   const handleSubmit = (event) => {
-    // The default action when a form is submitted is that the page will refresh
-    // To prevent that default behavior, add the following line
     event.preventDefault();
 
     const blog = { title, body, author };
     setIsPending(true);
-    // JSON server will automatically add the unique id to the newly added item
     fetch("http://localhost:8000/blogs", {
       method: "POST",
       headers: {
@@ -22,13 +23,18 @@ const Create = () => {
       body: JSON.stringify(blog),
     }).then(() => {
       setIsPending(false);
+      // Go back 1 page i.e. return to the immediately previous page
+      // To go forward, just provide positive integers
+      // history.go(-1);
+      
+      // Redirecting to the "/" route
+      history.push("/");
     });
   };
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      {/*  */}
       <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
@@ -51,7 +57,6 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        {/* A button inside a form when pressed will fire a submit event by default */}
         {!isPending && <button>Add Blog</button>}
         {isPending && <button disabled>Adding Blog...</button>}
       </form>

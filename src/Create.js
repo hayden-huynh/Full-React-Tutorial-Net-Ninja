ecc97c2bf1ca30a2549d6b83ea74519274672bcf
Hayden Huynh <huynhhai323@gmail.com>
2021-08-02 14:42:55 +0700

    Finished tutorials 28 and 29

--- src/Create.js
+++ src/Create.js
@@ -3,34 +3,57 @@ import { useState } from "react";
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const [author, setAuthor] = useState("mario")
+  const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (event) => {
+    // The default action when a form is submitted is that the page will refresh
+    // To prevent that default behavior, add the following line
+    event.preventDefault();
+
+    const blog = { title, body, author };
+    setIsPending(true);
+    // JSON server will automatically add the unique id to the newly added item
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setIsPending(false);
+    });
+  };
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      {/*  */}
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
           required
-          value={title} // Set the value of the input to equal the state
-          onChange={(event) => setTitle(event.target.value)} // Update the state on every changes made to the value of the input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
         />
         <label>Blog body:</label>
-        <textarea 
+        <textarea
           required
           value={body}
           onChange={(event) => setBody(event.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select 
+        <select
           value={author}
           onChange={(event) => setAuthor(event.target.value)}
         >
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {/* A button inside a form when pressed will fire a submit event by default */}
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
